71c6119fdbffc46cff044e15e3a780d9f635ed95
Bogdan <bohdansadovskiy@gmail.com>
2023-12-13 10:27:31 +0200

    added logs

--- Fight Logs.cs
+++ Fight Logs.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
-namespace GAME_HEROES
+namespace myGame
 {
     internal class Fight_Logs
     {
+        public void LOGSWriter(string log)
+        {
+          
+        }
+        public string LOGS(Hero player1, Hero player2, string winner, int rounds)
+        {
+            string fightLog = "";
+            fightLog += "Player 1(" + player1.Name + ") VS Player 2(" + player2.Name;
+            fightLog += "\nWinner - " + winner;
+            fightLog += "\nNumber of rounds - " + rounds;
+            fightLog += "\nPlayer 1:\nDamage dealt (" + player1.damageDealt.ToString() + "); Damage recived ("
+                + player1.damageRecived.ToString() + "); Healed (" + player1.healed.ToString()+ "\n";
+            fightLog += "\nPlayer 2:\nDamage dealt (" + player2.damageDealt.ToString() + "); Damage recived ("
+                + player2.damageRecived.ToString() + "); Healed (" + player2.healed.ToString() + "\n";
+            return fightLog;
+        }
+
     }
 }
